0c83aefb1d19890a688e2d6ac8ed95bc83b3d95e
399384 <399384@916-IAM-134-11>
2023-09-07 21:03:42 -0500

    Add project files.

---
+++ Week1DungeonCrawler/Game.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.ConsoleColor;
+
+namespace Week1DungeonCrawler
+{
+    public class Game
+    {
+        string title = "Welcome To Candy Castle Run!";
+        string objective = "You have three lives to pass three levels to make it through the Candy Castle. Each level asks one question that you must answer correctly to reach the nect level. If you answer incorrectly, "
+        public void Start()
+        {
+            SetTextColor(Magenta);
+            WriteLine(title);
+            resetColor();
+
+            WriteLine("What is your name?");
+            string s = ReadLine();
+            Player p = new Player(s, 1, 3);
+            Write("Welcome ");
+            SetTextColor(Cyan);
+            Write(p.getName());
+            resetColor();
+            WriteLine("!");
+
+
+        }
+
+        public void SetTextColor(ConsoleColor c)
+        {
+            ForegroundColor = c;
+        }
+
+        public void resetColor()
+        {
+            ForegroundColor = White;
+        }
+    }
+}
---
+++ Week1DungeonCrawler/Player.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1DungeonCrawler
+{
+    internal class Player
+    {
+        string Name { get; set; }
+        int Level { get; set; }
+        int Lives { get; set; }
+
+        public Player()
+        {
+            Name = "Player";
+            Level = 1;
+            Lives = 3;
+        }
+
+        public Player(string n, int le, int li)
+        {
+            Name = n;
+            Level = le;
+            Lives = li;
+        }
+
+        public string getName()
+        {
+            return Name;
+        }
+
+        public int getLives()
+        {
+            return Lives;
+        }
+
+        public int getLevels()
+        {
+            return Level;
+        }
+    }
+}
